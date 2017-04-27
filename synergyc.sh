7ea4ce7a16da7a388b98b5dc8ad2e3262dbf757d
andyfitzgerald <andy@aiincorporated.com>
2017-04-27 13:25:43 -0400

    Added debugging

--- synergyc.sh
+++ synergyc.sh
@@ -3,10 +3,19 @@
 source utils.sh
 source config.sh
 
+_VERBOSE=1
+
 if [[ "$(getOsVers)" == "16.04" ]]; then
 	getPackages "synergy"
 else
 	log "Unrecognized OS version. Not installed pre-requisites."
 fi
 
+log "remoteSshAddr=$remoteSshAddr"
+log "remoteSshPort=$remoteSshPort"
+log "remoteSshUser=$remoteSshUser"
+log "fwdPortLocal=$fwdPortLocal"
+log "fwdPortRemote=$fwdPortRemote"
+
 ssh -f -N -4 -C -c aes256-gcm@openssh.com -L ${fwdPortLocal}:localhost:${fwdPortRemote} -p ${remoteSshPort} "${remoteSshUser}@${remoteSshAddr}"
+synergy
