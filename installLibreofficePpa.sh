dfb68e818ebae67046a756c4ba140a12ff1b2d16
andyfitzgerald <andy@aiincorporated.com>
2017-05-18 08:24:56 -0400

    Assume yes for apt-get commands. Reordered packages for readability.

--- installLibreofficePpa.sh
+++ installLibreofficePpa.sh
@@ -8,21 +8,21 @@ sudo apt-get clean
 sudo apt-get autoremove
 
 # Add PPA for "LibreOffice fresh", the latest release of the newest series (but no alpha/beta releases)
-sudo add-apt-repository ppa:libreoffice/ppa
+sudo add-apt-repository --yes ppa:libreoffice/ppa
 sudo apt-get update
 
 if [[ "$(getOsVers)" == "16.04" ]]; then
-	sudo apt-get install \
-		libreoffice-kde\
+	sudo apt-get --yes install \
 		libreoffice-calc\
 		libreoffice-draw\
+		libreoffice-kde\
+		libreoffice-impress\
 		libreoffice-help-en-us\
 		libreoffice-math\
-		libreoffice-templates\
-		libreoffice-writer\
-		libreoffice-impress\
 		libreoffice-pdfimport\
 		libreoffice-style-*
+		libreoffice-templates\
+		libreoffice-writer\
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
