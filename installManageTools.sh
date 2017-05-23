3cdc74ea884a96fc6b41758eb78645c60c92c50a
andyfitzgerald <andy@aiincorporated.com>
2017-05-23 15:15:38 -0400

    Fixed tabbing. Adding remove for existing version.

--- installManageTools.sh
+++ installManageTools.sh
@@ -6,8 +6,8 @@ tempdir=$(mktemp -d)
 
 # Function to cleanup
 function cleanup () {
-        log "Deleting temp directory: $tempdir"
-        rm -rf "$tempdir"
+	log "Deleting temp directory: $tempdir"
+	rm -rf "$tempdir"
 }
 trap cleanup EXIT
 
@@ -15,21 +15,23 @@ trap cleanup EXIT
 _VERBOSE=1
 
 function getGanttProject () {
-        local url="http://www.ganttproject.biz/dl/2.8.5/lin"
-        local filename="ganttproject.deb"
-        local filenameAbs="${tempdir}/${filename}"
-        
-        wget \
-                --directory-prefix="$tempdir" \
-                --output-document="$filenameAbs" \
-                "$url"
-                
-        #local installFile="$(find -iname "*.deb" -print -quit)"
-        sudo dpkg --install "$filenameAbs"  | log
+	# Uninstall existing version
+	sudo apt-get remove --yes ganttproject
+
+	local url="http://www.ganttproject.biz/dl/2.8.5/lin"
+	local filename="ganttproject.deb"
+	local filenameAbs="${tempdir}/${filename}"
+
+	wget \
+		--directory-prefix="$tempdir" \
+		--output-document="$filenameAbs" \
+		"$url"
+
+	sudo dpkg --install "$filenameAbs"  | log
 }
 
 if [[ "$(getOsVers)" == "16.04" ]]; then
-	getGanttProject 
+	getGanttProject
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
