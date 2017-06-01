9c0896e2b73b07b05b5bfde78dbaf122359fd26d
andyfitzgerald <andy@aiincorporated.com>
2017-06-01 08:55:36 -0400

    Created new script to copy one block device to another more efficiently on modern Linux.

--- utils.sh
+++ utils.sh
@@ -97,3 +97,14 @@ trim()
     var="${var%"${var##*[![:space:]]}"}"
     echo -n "$var"
 }
+
+# Ref: http://unix.stackexchange.com/a/259254
+bytesToHuman() {
+	b=${1:-0}; d=''; s=0; S=(Bytes {K,M,G,T,E,P,Y,Z}iB)
+	while ((b > 1024)); do
+		d="$(printf ".%02d" $((b % 1024 * 100 / 1024)))"
+		b=$((b / 1024))
+		let s++
+	done
+	echo "$b$d ${S[$s]}"
+}
