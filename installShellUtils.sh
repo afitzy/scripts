d2c075256069bdfe1b135de51a3f198459ed6614
Andy Fitzgerald <afitzy@gmail.com>
2017-06-01 21:21:51 -0400

    Added fortunes

--- installShellUtils.sh
+++ installShellUtils.sh
@@ -7,6 +7,9 @@ _VERBOSE=1
 if [[ "$(getOsVers)" == "16.04" ]]; then
 	# pv: Monitor the progress of data through a pipe
 	getPackages "pv"
+
+	# fortunes
+	getPackages "fortune-mod" "fortunes" "fortunes-min" "fortunes-off" "fortunes-spam" "cookietool"
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
