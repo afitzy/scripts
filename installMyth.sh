73952585dda9e05698af95b3a40aead735a290f5
andyfitzgerald <andy@aiincorporated.com>
2017-06-02 22:18:52 -0400

    Adding todo

--- installMyth.sh
+++ installMyth.sh
@@ -4,8 +4,10 @@ source utils.sh
 
 # Install MythTv using Mythbuntu PPA
 # Installs:
-# 1) Complete frontend and backend system with database
-# 2) Plugins for a frontend system
+# * Complete frontend and backend system with database
+# * Plugins for a frontend system
+# ...
+# To-do: https://www.mythtv.org/wiki/Installing_MythTV_on_Ubuntu
 function installMythtv () {
 	sudo add-apt-repository --yes ppa:mythbuntu/0.28
 	sudo apt-get update
