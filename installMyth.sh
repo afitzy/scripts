eebb121b63b60420344f37f77597f811ebaed004
andyfitzgerald <andy@aiincorporated.com>
2017-06-04 13:29:20 -0400

    Added comment

--- installMyth.sh
+++ installMyth.sh
@@ -12,6 +12,11 @@ function installMythtv () {
 	sudo add-apt-repository --yes ppa:mythbuntu/0.28
 	sudo apt-get update
 	getPackages "mythtv" "mythplugins"
+
+	# Database Configuration
+	# If you get a message saying that Myth could not connect to the database, check that the password on the screen matches the one in /etc/mythtv/mysql.txt
+	# sudo cat /etc/mythtv/mysql.txt
+	# If there is a mismatch, change the one on the screen to match.
 }
 
 
