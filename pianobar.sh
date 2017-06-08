8821674c7d424da808e91f9d36aa4b520c418a5f
andyfitzgerald <andy@aiincorporated.com>
2017-06-08 07:35:21 -0400

    Adding prereq installation flag.

--- pianobar.sh
+++ pianobar.sh
@@ -4,7 +4,7 @@ source utils.sh
 
 # NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install this
 # separately; see below.
-PARSED="$(getopt --options vdi: --long "verbose,debug,startIdx:" -n 'pianobar.sh' -- "$@")"
+PARSED="$(getopt --options vdi: --long "verbose,debug,install,startIdx:" -n 'pianobar.sh' -- "$@")"
 
 if [ $? != 0 ] ; then
 	log "getopt has complained about wrong arguments to stdout"
@@ -17,22 +17,26 @@ eval set -- "$PARSED"
 
 _VERBOSE=0
 _DEBUG=0
+_INSTALL=0
 _START_IDX=1
 while true; do
 	case "$1" in
 		-v | --verbose ) _VERBOSE=1; shift ;;
 		-d | --debug ) _DEBUG=1; shift ;;
+		--install ) _INSTALL=1; shift ;;
 		-i | --startIdx ) _START_IDX="$2"; shift 2 ;;
 		-- ) shift; break ;;
 		* ) break ;;
 	esac
 done
 
-# Install prereqs for getFreeUsProxies.py
-getPackages "python-pip"
-getPythonPackages "enum34" "requests" "beautifulsoup4"
-proxies=("$(getFreeUsProxies.py --max=1 --port=80 --startIdx=${_START_IDX})")
+if [[ $_INSTALL -eq 1 ]]; then
+	# Prereqs for getFreeUsProxies.py
+	getPackages "python-pip"
+	getPythonPackages "enum34" "requests" "beautifulsoup4"
+fi
 
+proxies=("$(getFreeUsProxies.py --max=1 --port=80 --startIdx=${_START_IDX})")
 for proxy in ${proxies[@]}; do
 	echo "Trying proxy $proxy"
 
