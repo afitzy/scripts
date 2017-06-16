8425f1f3f4b7c72ab8ff9e49077224136529bd15
Katharine Fitzgerald <katharine.mcclellan@gmail.com>
2017-06-16 15:07:02 -0400

    Adding a few common libraries.

--- installPythonUtils.sh
+++ installPythonUtils.sh
@@ -7,7 +7,7 @@ _VERBOSE=1
 if [[ "$(getOsVers)" == "16.04" ]]; then
 	# python2
 	getPackages "python-pip" "ipython" "python-dateutil"
-	getPythonPackages "money" "Babel" "bs4" "lxml"
+	getPythonPackages "money" "Babel" "bs4" "lxml" "requests" "enum34" "beautifulsoup4"
 
 	# python3
 	getPackages "python3-pip" "ipython3" "python3-dateutil"
