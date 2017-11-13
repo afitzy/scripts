06a53fb13b2a98fe79c623128f5234d173b2431b
andyfitzgerald <andy@aiincorporated.com>
2017-11-13 07:33:49 -0500

    Adding translator CLI installation.

--- installs/installTranslate.sh
+++ installs/installTranslate.sh
@@ -2,8 +2,21 @@
 
 source ../utils.sh
 
+function installTranslateRepo ()
+{
+	local repoAddr="https://github.com/soimort/translate-shell.git"
+	local instPrefix=_DIR_PREFIX
+
+	cloneGitRepo "$_DIR_SRC" "$repoAddr"
+	sudo make install PREFIX="$instPrefix"
+	popd
+	popd
+}
+
+
 if [[ "$(getOsVers)" == "16.04" ]]; then
-	getPackage translate-shell
+	#getPackage translate-shell
+	installTranslateRepo
 else
 	echo "Unrecognized OS version. Not installing."
 fi
