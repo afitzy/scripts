337943afeb8b978cac25db09d9e3afec2215cb91
Andy Fitzgerald <afitzy@gmail.com>
2017-12-28 16:28:20 -0500

    Samba user configuration initial revision.

---
+++ config/addSambaUser.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+scriptName="$(basename "$0")"
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dateStamp=$(date --iso-8601="seconds")
+
+source ../utils.sh
+
+sambaGroup="sambashare"
+username="$1"
+
+if [[ "$(getOsVers)" == "16.04" ]]; then
+	# Create an OS user
+	sudo adduser \
+		--no-create-home \
+		--disabled-password \
+		--disabled-login \
+		--shell /bin/false \
+		--groups "${sambaGroup}" \
+		"$username"
+
+	# Create a samba user
+	sudo smbpasswd -a "$username"
+
+	# Restart the samba server
+	sudo service smbd force-reload
+else
+	echo "Unrecognized OS version. Not installed pre-requisites."
+fi
