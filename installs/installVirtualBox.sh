6cdc971614521525673840829f73b03498a60a56
Katharine Fitzgerald <katharine.mcclellan@gmail.com>
2018-02-10 14:27:18 -0500

    Moved remove into function. Added install from old multiverse instructions.

--- installs/installVirtualBox.sh
+++ installs/installVirtualBox.sh
@@ -6,16 +6,27 @@ dateStamp=$(date --iso-8601="seconds")
 
 source "${scriptDir}/../utils.sh"
 
+function removeVirtualBox ()
+{
+	# Remove existing
+	sudo apt-get remove --purge virtualbox*
+	sudo apt-get clean
+	sudo apt-get autoremove
+}
+
+function installVitualBoxFromMultiverse ()
+{
+	removeVirtualBox
+	sudo apt-get install --yes virtualbox virtualbox-ext-pack virtualbox-guest-* virtualbox-qt
+}
+
 # The binaries in this repository are all released under the VirtualBox Personal
 # Use and Evaluation License (PUEL). By downloading, you agree to the terms and
 # conditions of that license.
 # Ref: https://www.ubuntuupdates.org/ppa/virtualbox.org_contrib
 function installVirtualBoxFromOracle ()
 {
-	# Remove existing
-	sudo apt-get remove --purge virtualbox*
-	sudo apt-get clean
-	sudo apt-get autoremove
+	removeVirtualBox
 
 	# Add Oracle key
 	local url='http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc'
@@ -26,7 +37,8 @@ function installVirtualBoxFromOracle ()
 	sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian ${ubuntuRelease} non-free contrib' > /etc/apt/sources.list.d/virtualbox.org.list"
 
 	sudo apt-get update
-	sudo apt-get install --yes virtualbox-5.2 virtualbox-ext-pack virtualbox-guest-* virtualbox-qt
+	sudo apt-get install --yes virtualbox-5.2
+	# virtualbox-ext-pack virtualbox-guest-* virtualbox-qt
 }
 
 
