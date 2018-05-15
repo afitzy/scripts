aec6acf5dc241f63457ddfc77453cb619c2f8175
Andy Fitzgerald <afitzy@gmail.com>
2018-05-14 20:40:31 -0400

    Adding pyexcel_ods

--- installs/installPythonUtils.sh
+++ installs/installPythonUtils.sh
@@ -11,11 +11,11 @@ _VERBOSE=1
 if [[ "$(getOsVers)" == "16.04" ]]; then
 	# python2
 	getPackages "python-pip" "ipython" "python-dateutil"
-	getPythonPackages "money" "Babel" "bs4" "lxml" "requests" "enum34" "beautifulsoup4"
+	getPythonPackages "money" "Babel" "bs4" "lxml" "requests" "enum34" "beautifulsoup4" "pyexcel_ods"
 
 	# python3
 	getPackages "python3-pip" "ipython3" "python3-dateutil"
-	getPythonPackages "pyoctopart" "dateutil" "money"
+	pip3 install "pyoctopart" "money" # "dateutil"
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
