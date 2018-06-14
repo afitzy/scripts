3e7ef0a550c443ecb17a1e8b93c7e14854064cf0
andyfitzgerald <andy@aiincorporated.com>
2018-06-14 07:43:27 -0400

    Adding pdfsandwich, tesseract w/ language packs, and unpaper.

--- installs/installPdfUtils.sh
+++ installs/installPdfUtils.sh
@@ -16,7 +16,16 @@ trap cleanup EXIT
 _VERBOSE=1
 
 if [[ "$(getOsVers)" == "16.04" ]]; then
-	sudo apt-get install --yes ocaml unpaper tesseract
+	# pdfsandwich generates "sandwich" OCR pdf files
+	# http://www.tobias-elze.de/pdfsandwich/
+	sudo apt-get install --yes pdfsandwich
+
+	# OCR tool and language packs
+	sudo apt-get install --yes tesseract-ocr tesseract-ocr-eng tesseract-ocr-chi-sim tesseract-ocr-chi-tra
+
+	# unpaper is a post-processing tool for scanned sheets of paper.
+	# Purpose is to make scanned book pages better readable on screen after conversion to PDF
+	sudo apt-get install --yes unpaper
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
