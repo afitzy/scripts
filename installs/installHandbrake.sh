8f2cedbd7d8b5d771ffbb31e3cd6ed5027a98743
Andy Fitzgerald <afitzy@gmail.com>
2019-01-12 20:59:46 -0500

    Fixed missing variables.

--- installs/installHandbrake.sh
+++ installs/installHandbrake.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+scriptName="$(basename "$0")"
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dateStamp=$(date --iso-8601="seconds")
+
 source "${scriptDir}/../utils.sh"
 
 function getHandbrake () {
