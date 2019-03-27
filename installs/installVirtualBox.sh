7ef7826d81ab24fd0acbabaf525afd148f14739b
andyfitzgerald <andy@aiincorporated.com>
2019-03-27 09:15:02 -0400

    Updated to use VirtualBox 6.0.

--- installs/installVirtualBox.sh
+++ installs/installVirtualBox.sh
@@ -8,9 +8,10 @@ source "${scriptDir}/../utils.sh"
 
 function removeVirtualBox ()
 {
-	# Remove existing
+	local -r friendlyName="VirtualBox"
+	echo "${friendlyName}: removing previous installation"
 	sudo apt-get remove --purge virtualbox*
-	sudo apt-get clean 
+	sudo apt-get clean
 	sudo apt-get autoremove --yes
 }
 
@@ -26,27 +27,48 @@ function installVitualBoxFromMultiverse ()
 # Ref: https://www.ubuntuupdates.org/ppa/virtualbox.org_contrib
 function installVirtualBoxFromOracle ()
 {
-	removeVirtualBox
+	local -r removeOldVersion="$1"
+	local -r version="${2:-1}"
 
-	# Add Oracle key
+	local -r friendlyName="VirtualBox"
+
+	if [[ "$removeOldVersion" -eq 1 ]]; then
+		removeVirtualBox
+	fi
+
+	echo "${friendlyName}: downloading Oracle key"
 	local url='http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc'
 	wget --quiet --output-document=- ${url} | sudo apt-key add -
 
-	# Set up repository
+	echo "${friendlyName}: setting up repository"
 	ubuntuRelease=$(lsb_release -cs)
 	sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian ${ubuntuRelease} non-free contrib' > /etc/apt/sources.list.d/virtualbox.org.list"
 
+	echo "${friendlyName}: refreshing multiverse"
 	sudo apt-get update
-	sudo apt-get install --yes virtualbox-5.2
+
+	echo "${friendlyName}: installing"
+	sudo apt-get install --yes virtualbox-${version}
 
 	# These conflict with virtualbox-5.2
 	# sudo apt-get install --yes virtualbox-ext-pack virtualbox-guest-* virtualbox-qt
 }
 
+function installVirtualBoxFromOracle_v5.2 () {
+	local -r removeOldVersion=1
+	local -r version=5.2
+	installVirtualBoxFromOracle "$removeOldVersion" "$version"
+}
+
+function installVirtualBoxFromOracle_v6.0 () {
+	local -r removeOldVersion=1
+	local -r version=6.0
+	installVirtualBoxFromOracle "$removeOldVersion" "$version"
+}
 
 _VERBOSE=1
 if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" ]]; then
-	installVirtualBoxFromOracle
+	installVirtualBoxFromOracle_v6.0
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
