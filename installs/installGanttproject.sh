c0abe29b8c143ec6a1e4dc81a85bb30201a3b8ec
Andy Fitzgerald <andy@aiincorporated.com>
2019-04-25 19:39:20 -0400

    Split manage tools into Gantt project and PlantUML.

---
+++ installs/installGanttproject.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+scriptName="$(basename "$0")"
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dateStamp=$(date --iso-8601="seconds")
+
+source "${scriptDir}/../utils.sh"
+
+tempdir=$(mktemp -d)
+
+# Function to cleanup
+function cleanup () {
+	log "Deleting temp directory: $tempdir"
+	rm -rf "$tempdir"
+}
+trap cleanup EXIT
+
+
+_VERBOSE=1
+
+function getGanttProject () {
+	# Uninstall existing version
+	sudo apt-get remove --yes ganttproject
+
+	local url="http://www.ganttproject.biz/dl/2.8.5/lin"
+	local filename="ganttproject.deb"
+	local filenameAbs="${tempdir}/${filename}"
+
+	wget \
+		--directory-prefix="$tempdir" \
+		--output-document="$filenameAbs" \
+		"$url"
+
+	sudo dpkg --install "$filenameAbs"  | log
+}
+
+if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" ]]; then
+	getGanttProject
+else
+	echo "Unrecognized OS version. Not installed pre-requisites."
+fi
