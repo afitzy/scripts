91fb8328094be0fc0b463940461f3c693e362a54
Andy Fitzgerald <andy@aiincorporated.com>
2019-10-28 08:48:32 -0400

    Adding json to parse-able extensions

--- grep.bash
+++ grep.bash
@@ -59,7 +59,7 @@ function grepsrc () {
 	local declare path="$1"; shift;
 	local declare fargs="-type f"
 	local declare gargs="--no-messages --with-filename --line-number --color=always"
-	local declare exts=(h c hpp cpp mkdep makefile mak mk config asm inc cmd idl sh bash)
+	local declare exts=(h c hpp cpp mkdep makefile mak mk config asm inc cmd idl sh bash json)
 	local declare files=(makefile make.rules)
 	local declare ignores=(.git .svn .settings)
 	local fcmd="$(python -c "$buildFindCmd" --args "${fargs}" --path "${path}" --exts ${exts[@]} --files ${files[@]} --ignores ${ignores[@]} --quote=\')"
