1cd06b28b9071bea13070e56b1b3ecc2d2813e3a
Andy Fitzgerald <andy@aiincorporated.com>
2020-12-01 14:57:30 -0500

    Add steps to manually install latest HP software direct from HP

--- installs/installPrintScanUtils.sh
+++ installs/installPrintScanUtils.sh
@@ -8,6 +8,23 @@ source "${scriptDir}/../utils.sh"
 
 _VERBOSE=1
 
+# Ref: https://askubuntu.com/a/1056078
+function uninstallHpTools ()
+{
+	sudo apt-get purge \
+		hplip \
+		hplip-data \
+		hplip-doc \
+		hplip-gui \
+		hpijs-ppds \
+		libsane-hpaio \
+		printer-driver-hpcups \
+		printer-driver-hpijs
+
+	sudo rm -rf /usr/share/hplip/
+	sudo apt-get autoremove
+}
+
 function installHpTools ()
 {
     sudo apt-get install --yes hplip
@@ -16,9 +33,20 @@ function installHpTools ()
     sudo ln -f -s /usr/share/hplip/sendfax.py /usr/bin/hp-sendfax
 }
 
+# Ref: https://developers.hp.com/hp-linux-imaging-and-printing/gethplip
+function installHpToolsFromHp ()
+{
+	wget https://download.sourceforge.net/project/hplip/hplip/3.20.11/hplip-3.20.11.run
+	sh hplip-3.20.11.run
+}
+
 if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" ]]; then
-	installHpTools
-	sudo apt-get install --yes xsane ksaneplugin
+	uninstallHpTools
+
+	sudo apt-get install --fix-missing --yes xsane ksaneplugin
+
+	# installHpTools
+	installHpToolsFromHp
 
 	# GUI tools
 	sudo apt-get install --yes skanlite
