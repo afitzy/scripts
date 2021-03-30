fc0bbdb8ed5ad968759773a6e78f5479d615a753
Andy Fitzgerald <afitzy@gmail.com>
2021-03-29 21:01:13 -0400

    Remove "

--- installs/installAudioTools.sh
+++ installs/installAudioTools.sh
@@ -10,10 +10,10 @@ _VERBOSE=1
 
 if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" || "$(getOsVers)" == "20.04" ]]; then
 	# GUI tools
-	sudo apt-get install --yes "audacity"
+	sudo apt-get install --yes audacity
 
 	# CLI tools
-	sudo apt-get install --yes "silan" "mp3val"
+	sudo apt-get install --yes silan mp3val
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
