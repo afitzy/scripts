00e53e478cacaa8910edbfdc4f5d7c9692f16cdf
Andy Fitzgerald <afitzy@gmail.com>
2021-03-29 20:52:27 -0400

    Add suggestions

--- installs/installConky.sh
+++ installs/installConky.sh
@@ -14,7 +14,7 @@ source "${scriptDir}/../utils.sh"
 _VERBOSE=1
 
 if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" || "$(getOsVers)" == "20.04" ]]; then
-	sudo apt-get install conky conky-all lua5.1 hddtemp smartmontools thermald lm-sensors
+	sudo apt-get install --install-suggests conky conky-all lua5.1 hddtemp smartmontools thermald lm-sensors
 	conky -c ~/.conky/.conkyDateTime &
 	conky -c ~/.conky/.conkySysStats &
 	conky -c ~/.conky/.conkyFortune &
