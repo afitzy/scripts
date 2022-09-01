9fed8afd716ac566619f4186e97c828b0ea67daa
Andy Fitzgerald <afitzy@gmail.com>
2022-09-01 08:18:03 -0400

    Add capability to install ganttproject 3.2

--- installs/installGanttproject.sh
+++ installs/installGanttproject.sh
@@ -18,7 +18,8 @@ trap cleanup EXIT
 
 _VERBOSE=1
 
-function getGanttProject () {
+# Last version before cloud capabilities were added
+function getGanttProject2.8.11 () {
 	# Uninstall existing version
 	sudo apt-get remove --yes ganttproject
 
@@ -34,8 +35,25 @@ function getGanttProject () {
 	sudo dpkg --install "$filenameAbs"  | log
 }
 
+# ganttproject3.2 is super-buggy. Strongly suggest not to use it.
+function getGanttProject3.2 () {
+	# Uninstall existing version
+	sudo apt-get remove --yes ganttproject
+
+	local url="https://github.com/bardsoftware/ganttproject/releases/download/ganttproject-3.2.3200/ganttproject_3.2.3230-1_all.deb"
+	local filename="ganttproject_3.2.3230-1_all.deb"
+	local filenameAbs="${tempdir}/${filename}"
+
+	wget \
+		--directory-prefix="$tempdir" \
+		--output-document="$filenameAbs" \
+		"$url"
+
+	sudo dpkg --install "$filenameAbs"  | log
+}
+
 if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" || "$(getOsVers)" == "20.04" ]]; then
-	getGanttProject
+	getGanttProject2.8.11
 else
 	echo "Unrecognized OS version. Not installed pre-requisites."
 fi
