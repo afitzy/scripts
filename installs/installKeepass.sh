741d63813d1cee672bbc7d7655c1e5057b687255
Andy Fitzgerald <afitzy@gmail.com>
2025-09-14 10:04:25 -0400

    Adding keepass installation

---
+++ installs/installKeepass.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+scriptName="$(basename "$0")"
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dateStamp=$(date --iso-8601="seconds")
+
+source "${scriptDir}/../utils.sh"
+
+
+
+_VERBOSE=1
+if [[ "$(getOsVers)" == "16.04" || "$(getOsVers)" == "18.04" || "$(getOsVers)" == "20.04" || "$(getOsVers)" == "22.04" ]]; then
+	sudo apt-get install keepassx
+elif [[ "$(getOsVers)" == "24.04" ]]; then
+	sudo apt-get install keepassxc
+else
+	echo "Unrecognized OS version. Not installed pre-requisites."
+fi
